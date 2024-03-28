8469d5809454a610516a25247b2f15f60d330800
ValerioCarbone <valeriocarbone10@gmail.com>
2024-03-28 14:26:00 +0100

    First snack

---
+++ src/org/experis/snacks/Snack1.java
@@ -0,0 +1,28 @@
+package org.experis.snacks;
+
+import java.util.Scanner;
+
+public class Snack1 {
+    public static void main(String[] args) {
+
+        Scanner scan = new Scanner(System.in);
+
+        int givenNumber;
+
+
+        // codice da eseguire
+      do {
+          System.out.println("Pick a number between 10 and 20");
+
+          givenNumber = Integer.parseInt(scan.nextLine());
+
+          System.out.println(givenNumber);
+
+      } // condizione per ripetere il ciclo
+      while (givenNumber > 20 || givenNumber < 10);
+
+        System.out.println("The number is correct!");
+
+        scan.close();
+    }
+}
